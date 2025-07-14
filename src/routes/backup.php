866adb9def604e4e57fa3224aebcfda49ba460c5
thisisharshoriya5565 <thisisharshoriya@gmail.com>
2025-07-14 12:44:27 +0530

    route issue fixed

--- src/routes/backup.php
+++ src/routes/backup.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Vendor\DbBackup\Http\Controllers\BackupController;
 
-Route::middleware(config('config/dbbackup.php'))->prefix('backup')->name('backup.')->group(function () {
+Route::middleware(config('dbbackup.middleware'))->prefix('backup')->name('backup.')->group(function () {
     Route::get('/', [BackupController::class, 'index'])->name('index');
     Route::get('/list', [BackupController::class, 'list'])->name('list');
     Route::get('/restore/{folder}', [BackupController::class, 'restore'])->name('restore');
